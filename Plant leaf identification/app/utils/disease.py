fa037ff5755ec8e6a4f97fe31a369ec88ed04652
Suprit Yentruvi <Suprityentruvi2003@gmail.com>
2024-07-10 10:03:22 +0530

    Initial commit

---
+++ Plant leaf identification/app/utils/disease.py
@@ -0,0 +1,235 @@
+disease_dic = {
+    'Apple': """ <b>Leaf idetification</b>: Apple <br/><br/>
+        <br/> 
+
+        
+
+        
+
+       
+        
+      .""",
+
+    'Apple': """ <b>leaf </b>: Apple <br/>
+        <br/> 
+
+       
+        <br/>.""",
+
+    'Apple': """ <b>leaf </b>: Apple <br/>
+        <br/> 
+
+        
+        
+        <br/>.""",
+
+
+
+    'Apple': """ <b>leaf </b>: Apple <br/>
+
+        """,
+
+
+
+
+    'Blueberry': """ <b>leaf </b>: Blueberry <br/>""",
+
+
+
+
+    'Cherry': """ <b>leaf </b>: Cherry <br/>
+        <br/> 
+
+       
+        .""",
+
+    'Cherry': """ <b>leaf </b>: Cherry <br/>
+
+        """,
+
+
+    'Corn': """ <b>leaf </b>: Corn <br/>
+        <br/> 
+
+        
+
+        <br/>.""",
+
+
+
+
+    'Corn': """ <b>leaf </b>: Corn <br/>
+        <br/> 
+
+       
+       . """,
+
+
+    'Corn': """ <b>leaf </b>: Corn <br/>
+        <br/>
+        <br/> 
+
+       
+        <br/>.""",
+
+
+    'Grape': """ <b>leaf </b>: Grape <br/>
+        <br/> 
+
+       
+       when the fruit has set.""",
+
+    'Corn': """ <b>leaf </b>: Corn <br/>
+
+        <br/><br/> """,
+
+
+    'Grape': """ <b>leaf </b>: Grape <br/>
+        <br/> 
+
+       
+
+       .""",
+
+    'Grape': """ <b>leaf </b>: Grape <br/> Blight<br/>
+        <br/> 
+
+        """,
+
+    'Grape': """ <b>leaf </b>: Grape <br/>
+
+        <br/><br/> """,
+
+
+    'Corn': """ <b>leaf </b>: Corn(maize) <br/>
+
+        <br/><br/> """,
+
+    'Grape': """<b> Leaf</b> : Grape <br/>""",
+
+
+    'Orange': """ <b>leaf </b>: Orange <br/>""",
+
+
+
+    'Peach': """ <b>leaf </b>: Peach <br/><br/>
+        <br/> 
+
+       """,
+
+
+    'Pepper': """ <b>leaf </b>: Pepper <br/><br/>
+        <br/> 
+
+       """,
+
+    'Peach': """ <b>leaf </b>: Peach <br/>
+
+        <br/><br/> """,
+
+    'Pepper,': """ <b>leaf </b>: Pepper <br/>
+
+        <br/><br/> """,
+
+
+    'Potato': """ <b>leaf </b>: Potato <br/>
+
+        <br/><br/> """,
+
+    'Raspberry': """ <b>leaf </b>: Raspberry <br/>
+
+        <br/><br/> """,
+
+
+    'Soybean': """ <b>leaf </b>: 
+
+        <br/><br/> """,
+
+    'Strawberry': """ <b>leaf </b>: 
+
+        <br/><br/> """,
+
+    'Tomato': """ <b>leaf </b>:
+        <br/><br/> """,
+
+
+    'Potato': """ <b>leaf </b>:
+        <br/> 
+
+        """,
+
+
+    'Potato': """ <b>leaf </b>:
+
+        """,
+
+    'Squash': """ <b>leaf </b>: 
+        <br/> 
+
+       .""",
+
+
+    'Strawberry': """ <b>leaf </b>:
+        <br/> 
+
+      """,
+
+
+
+    'Tomato': """ <b>leaf </b>: 
+        <br/> 
+
+       """,
+
+
+
+    'Tomato': """ <b>leaf </b>:
+        <br/> 
+
+       .""",
+
+
+
+    'Tomato': """ <b>leaf </b>: 
+
+        .""",
+
+
+
+    'Tomato___disease_Mold': """ <b>leaf </b>: 
+        <br/> 
+
+       """,
+
+    'Tomato___Septoria_disease_spot': """ <b>leaf </b>:
+        <br/> 
+
+
+
+      .""",
+
+
+
+    'Tomato': """ <b>leaf </b>: Tomato <br/>
+        <br/> 
+
+   """,
+
+
+    'Tomato': """ <b>leaf </b>: Tomato <br/>
+        <br/> 
+
+        """,
+
+
+    'Tomato': """ <b>leaf </b>: Tomato <br/>        <br/> 
+""",
+
+
+
+
+    'Tomato': """ <b>leaf </b>: Tomato <br/>
+        <br/> 
+
+       """
+}
